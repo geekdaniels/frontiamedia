8aeb9128f81c771caa8f82f1f37d92e61fba7244
Olufemi Oladotun Daniel <oladotun.olufemi@yahoo.com>
2018-06-20 16:17:12 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function() {
+		var selectedClass = "";
+		$(".fil-cat").click(function(){ 
+		selectedClass = $(this).attr("data-rel"); 
+     $("#portfolio").fadeTo(100, 0.1);
+		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('scale-anm');
+      $("#portfolio").fadeTo(300, 1);
+    }, 300); 
+		
+	});
+});
